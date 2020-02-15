f27e0f8f0067df68f10b79a346ac4a6ef0270fd3
jli452 <jli452@binghamton.edu>
2020-02-15 16:32:17 -0500

    Command responsive, add pins next

---
+++ bot.js
@@ -0,0 +1,44 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+
+        args = args.splice(1);
+        switch(cmd) {
+            // !add
+            case 'add':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Added to chicken tender count.'
+                });
+            break;
+            // !remove
+            case 'reset':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Reset chicken tender count.'
+                });
+            break;
+         }
+     }
+});
