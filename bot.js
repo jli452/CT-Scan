1bc6e882a648643bc245c4b69299b6d7df2271ae
jli452 <jli452@binghamton.edu>
2020-02-15 20:09:25 -0500

    json writing functionality added

--- bot.js
+++ bot.js
@@ -1,6 +1,12 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+var fs = require('fs');
+
+var contents = fs.readFileSync('nums.json', 'utf8');
+var counter = JSON.parse(contents);
+var currentCounter = counter.currentCounter;
+var totalCounter = 0;
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -18,6 +24,7 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
+
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
@@ -27,16 +34,36 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         switch(cmd) {
             // !add
             case 'add':
+                currentCounter++;
+                let cc = {
+                  "currentCounter": currentCounter
+                };
+                let data = JSON.stringify(cc);
+                fs.writeFileSync('nums.json', data);
                 bot.sendMessage({
                     to: channelID,
-                    message: 'Added to chicken tender count.'
+                    message: 'Added to chicken tender count. Current count: ' + currentCounter
                 });
             break;
             // !remove
             case 'reset':
+                currentCounter = 0;
                 bot.sendMessage({
                     to: channelID,
                     message: 'Reset chicken tender count.'
+                });
+            break;
+            case 'remove':
+                currentCounter--;
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Removed from chicken tender count. Current count: ' + currentCounter
+                });
+            break;
+            case 'streak':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Currently he has eaten ' + currentCounter + " times of chicken tenders in a row."
                 });
             break;
          }
